7da2485180320cf325986c64022d3db162db4e5e
olishkevych <olishkevych.natalia@gmail.com>
2023-09-18 23:35:56 +0200

    add automatic auth re-routing in App

--- src/routes/RootNav.jsx
+++ src/routes/RootNav.jsx
@@ -7,42 +7,52 @@ import RegistrationScreen from "../Screens/RegistrationScreen";
 import CommentsScreen from "../Screens/CommentsScreen";
 import HeaderNav from "../components/HeaderNav";
 import MapScreen from "../Screens/MapScreen";
+import { useSelector } from "react-redux";
+import { selectIsAuthorized } from "../redux/selectors";
 
 const RootNav = () => {
+  const isAuthorized = useSelector(selectIsAuthorized);
   const Stack = createStackNavigator();
   return (
     <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Registration"
-        component={RegistrationScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="BottomNav"
-        component={BottomNav}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Comments"
-        component={CommentsScreen}
-        options={{
-          headerShown: true,
-          header: () => <HeaderNav title="Comments" />,
-        }}
-      />
-      <Stack.Screen
-        name="Map"
-        component={MapScreen}
-        options={{
-          headerShown: true,
-          header: () => <HeaderNav title="Map" />,
-        }}
-      />
+      {!isAuthorized ? (
+        <>
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Registration"
+            component={RegistrationScreen}
+            options={{ headerShown: false }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="BottomNav"
+            component={BottomNav}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Comments"
+            component={CommentsScreen}
+            options={{
+              headerShown: true,
+              header: () => <HeaderNav title="Comments" />,
+            }}
+          />
+          <Stack.Screen
+            name="Map"
+            component={MapScreen}
+            options={{
+              headerShown: true,
+              header: () => <HeaderNav title="Map" />,
+            }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
